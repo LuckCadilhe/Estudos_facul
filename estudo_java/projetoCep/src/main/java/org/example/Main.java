6981e277869808a30e7753b24baf5b8a3fe8bf7a
Lucas Cadilhe <lucas_cadilhe@hotmail.com>
2025-10-06 11:22:14 -0300

    Estudos python

---
+++ estudo_java/projetoCep/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+import com.google.gson.Gson;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+
+
+
+    }
+
+}
---
+++ estudo_java/projetoCep/src/main/java/org/example/endereco.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class endereco {
+        private String cep;
+        private String logradouro;
+        private String complemento;
+        private String bairro;
+        private String uf;
+}
