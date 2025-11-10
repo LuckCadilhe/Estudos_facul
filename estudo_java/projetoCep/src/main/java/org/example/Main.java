71fe74b32c34e5d49f1816b75424a1606e79d00d
Lucas Cadilhe <lucas_cadilhe@hotmail.com>
2025-11-10 20:09:44 -0300

    Estudo listas

--- estudo_java/projetoCep/src/main/java/org/example/Main.java
+++ estudo_java/projetoCep/src/main/java/org/example/Main.java
@@ -6,11 +6,17 @@ import okhttp3.Response;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class  buscaCep() {
     static void main() {
-
 
 
     }
 
+}
+class Endereco {
+    private String cep;
+    private String logradouro;
+    private String bairro;
+    private String localidade;
+    private String uf;
 }
--- estudo_java/projetoCep/src/main/java/org/example/endereco.java
+++ estudo_java/projetoCep/src/main/java/org/example/endereco.java
@@ -1,9 +1,9 @@
 package org.example;
 
-public class endereco {
-        private String cep;
-        private String logradouro;
-        private String complemento;
-        private String bairro;
-        private String uf;
+class endereco {
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String uf;
 }
